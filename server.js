962d9a963b26be1e0a013ca9945126d66d7b0d9b
Smeza314 <sebastianmeza314@gmail.com>
2021-04-19 13:35:27 -0700

    fix server port

--- server.js
+++ server.js
@@ -10,6 +10,4 @@ app.use(express.json())
 
 app.use(require('./routes'))
 
-require('./db')
-  .then(() => app.listen(process.env.PORT || 3000))
-  .catch(err => console.log(err))
+app.listen(process.env.PORT || 3000)
